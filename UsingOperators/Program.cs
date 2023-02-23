0514c7144e71940f1b13c1c436625272d04c2bb3
Jacob <46667568+jacobvd0@users.noreply.github.com>
2023-02-23 19:26:01 +1030

    Add files via upload

---
+++ UsingOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Transactions;
+
+namespace UsingOperators // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 90;
+            int bonusHealth = 10;
+            Console.WriteLine($"You have {health} heath.");
+            health += bonusHealth;
+            Console.WriteLine($"You now have {health} health (+{bonusHealth}).");
+            Console.ReadKey(true);
+        }
+    }
+}
